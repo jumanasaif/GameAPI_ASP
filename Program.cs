67ea7d274f725a59844f5018e45ff2fa5273d4db
Jumana Saif <jumanasaif2003@gmail.com>
2025-10-03 01:48:44 +0300

    Using DTOs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@ using WebApp1_GameStore.DTOs;
 var builder = WebApplication.CreateBuilder(args);
 
 
-var app = builder.Build(); 
+var app = builder.Build();
+
+List<GameDto> games = [
+    new(
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1999,7,15)
+    ),
+     new(
+        2,
+        "Final Fantasy XIV",
+        "RolePlaying",
+        59.99M,
+        new DateOnly(2010,9,30)
+    ),
+       new(
+        3,
+        "FIFA 23",
+        "Sports",
+        69.99M,
+        new DateOnly(2022,9,27)
+    ),
+];
 
 app.MapGet("/", () => "Hello World!");
 
