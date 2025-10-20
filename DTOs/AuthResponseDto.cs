b26d235230933c7d737f0fa8a4c8e4186fc6383a
Jumana Saif <jumanasaif2003@gmail.com>
2025-10-20 22:03:38 +0300

    Apply Authentication, Authorization and Repository Pattern

--- DTOs/AuthResponseDto.cs
+++
@@ -1,7 +0,0 @@
-namespace WebApp1_GameStore.DTOs;
-
-public record class AuthResponseDto(
-    string Token,
-    string UserName,
-    string Role
-);
--- DTOs/LoginDto.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace WebApp1_GameStore.DTOs;
-
-public record class LoginDto(
-    [Required] string UserName,
-    [Required] string Password
-);
